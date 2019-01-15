1b9dfcff6d3a257f49ea159a8a6a5f8a53839526
Marian Marian <fenrir55@gmail.com>
2019-01-15 19:27:16 +0100

    v1

--- GenericClass/Program.cs
+++ GenericClass/Program.cs
@@ -10,6 +10,18 @@ namespace GenericClass
     {
         static void Main(string[] args)
         {
+            Generic<int> generyczna = new Generic<int>();
+            Generic<string> generyczna2 = new Generic<string>();
+            generyczna.wypisz(1, 2);
+            generyczna2.wypisz("jeden", "dwa");
+        }
+    }
+    public class Generic<T>
+    {
+        public void wypisz(T a, T b)
+        {
+            Console.WriteLine(a + " " + b);
+            Console.ReadKey();
         }
     }
 }
